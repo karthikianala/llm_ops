b25968ab2a013773bc7129ad24160a582f263574
karthikianala <mohanakarthik.mk@gmail.com>
2024-05-13 12:26:47 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import MongoDashboard from "./MongoDashboard";
-import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { Box, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 
 export interface Filters {
   model: string;
@@ -31,32 +31,62 @@ function App() {
   };
 
   return (
-    <div>
-      <Select
-        labelId="model-select-label"
-        id="model-select"
-        value={filters.model}
-        label="Model"
-        onChange={handleModelChange}
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-end",
+        }}
       >
-        <MenuItem value="all">All</MenuItem>
-        <MenuItem value="haiku">Haiku</MenuItem>
-        <MenuItem value="sonet">Sonet</MenuItem>
-        <MenuItem value="claude2.1">Claude 2.1</MenuItem>
-      </Select>
-      <Select
-        labelId="performance-select-label"
-        id="performance-select"
-        value={filters.outputPerformance}
-        label="Output Performance"
-        onChange={handlePerformanceChange}
-      >
-        <MenuItem value="all">All</MenuItem>
-        <MenuItem value="good">Good</MenuItem>
-        <MenuItem value="bad">Bad</MenuItem>
-      </Select>
+        <Box
+          sx={{
+            width: "150px",
+            margin: "5px",
+            padding: "3px",
+            borderRadius: "5px",
+          }}
+        >
+          <Select
+            labelId="model-select-label"
+            id="model-select"
+            value={filters.model}
+            label="Model"
+            onChange={handleModelChange}
+            sx={{ width: "150px" }}
+          >
+            <MenuItem value="all">All</MenuItem>
+            <MenuItem value="haiku">Haiku</MenuItem>
+            <MenuItem value="sonet">Sonet</MenuItem>
+            <MenuItem value="claude2.1">Claude 2.1</MenuItem>
+          </Select>
+        </Box>
+        <Box
+          sx={{
+            width: "150px",
+            margin: "5px",
+            padding: "3px",
+            borderRadius: "5px",
+          }}
+        >
+          <Select
+            labelId="performance-select-label"
+            sx={{ width: "150px" }}
+            id="performance-select"
+            value={filters.outputPerformance}
+            label="Output Performance"
+            onChange={handlePerformanceChange}
+          >
+            <MenuItem value="all">All</MenuItem>
+            <MenuItem value="good">Good</MenuItem>
+            <MenuItem value="bad">Bad</MenuItem>
+          </Select>
+        </Box>
+      </Box>
+
       <MongoDashboard filters={filters} />
-    </div>
+    </>
   );
 }
 
